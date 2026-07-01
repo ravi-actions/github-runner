f59df4d2e75eabc3a984efc215eccb0d3b844cb7
ravitejaraju1999 <ravitejaraju1999@gmail.com>
2026-07-01 18:26:40 +0530

    secrets

--- main.tf
+++ main.tf
@@ -1,52 +1,49 @@
-data "aws_subnet" "public" {
-  id = "subnet-062f245a4abd51af8"
-}
+
 
 resource "aws_instance" "runner" {
   ami           = local.ami_id
   instance_type = "t3.small"
-
   vpc_security_group_ids = [aws_security_group.main.id]
-  subnet_id              = data.aws_subnet.public.id
+  subnet_id = "subnet-04522f5186c666253" #replace your Subnet
 
+  # need more for terraform
   root_block_device {
     volume_size = 50
-    volume_type = "gp3"
+    volume_type = "gp3" # or "gp2", depending on your preference
   }
-
   user_data = file("runner.sh")
-
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.environment}-runner"
+        Name = "${var.project}-${var.environment}-runner"
     }
   )
 }
 
 resource "aws_security_group" "main" {
-  name        = "${var.project}-${var.environment}-runner"
-  description = "Created to attach runner"
-  vpc_id      = "vpc-0618f6141d79da029"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name        =  "${var.project}-${var.environment}-runner"
+  description = "Created to attatch runner"
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.project}-${var.environment}-jenkins"
+        Name = "${var.project}-${var.environment}-jenkins"
     }
   )
 }
